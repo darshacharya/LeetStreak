aec628868969f9a0e0eb9aa1bdf48219852b0fe1
darshachrya <acharyasudarshan80@gmail.com>
2024-10-31 00:01:31 +0530

    added summary ranges

---
+++ Arrays_And_Strings/228. Summary Ranges.py
@@ -0,0 +1,24 @@
+"""228. Summary Ranges """
+"""Time: O(n)"""
+"""Space: O(n)"""
+
+nums = [1,2,4,5,7]
+
+def summaryRanges(nums):
+    ans = []
+    i = 0
+
+    while i < len(nums):
+        start = nums[i]
+        while i < len(nums) - 1 and nums[i] + 1 == nums[i + 1]:
+            i += 1
+
+        if start != nums[i]:
+            ans.append(str(start) + "->" + str(nums[i]))
+        else:
+            ans.append(str(nums[i]))
+
+        i += 1
+
+    return ans
+print(summaryRanges(nums))
