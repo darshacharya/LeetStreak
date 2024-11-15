8717de00687cfce2383d5d50d0c5123044634c7d
Sudarshan T S <chinnuacharya18@gmail.com>
2024-11-16 00:05:08 +0530

    Maximum Number of Balloons

---
+++ Hashmaps & Sets/1189. Maximum Number of Balloons.py
@@ -0,0 +1,18 @@
+"""1189. Maximum Number of Balloons"""
+
+
+def maxNumberOfBalloons(text):
+    counter = 0
+    balloon = "balloon"
+
+    for c in text:
+        if c in balloon:
+            counter[c] += 1
+
+    if any(c not in counter for c in balloon):
+        return 0
+    else:
+        return min(counter["b"], counter["a"], counter["l"] // 2, counter["o"] // 2, counter["n"])
+
+
+print(maxNumberOfBalloons("balloonnnsjalballon"))
