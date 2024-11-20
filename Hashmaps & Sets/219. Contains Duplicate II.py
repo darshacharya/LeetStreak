207a0b222590d9fa3b8055a628d13aefc24b00e6
Sudarshan T S <chinnuacharya18@gmail.com>
2024-11-20 23:20:48 +0530

    """219. Contains Duplicate II"""

---
+++ Hashmaps & Sets/219. Contains Duplicate II.py
@@ -0,0 +1,26 @@
+"""219. Contains Duplicate II"""
+
+
+
+def containsNearbyDuplicate(nums, k):
+    # i=0
+    # j=1
+    # for i in range(len(nums)-1):
+    #     for j in range(i+1,len(nums)):
+    #         if nums[i]==nums[j]:
+    #             if abs(i-j)<=k:
+    #                 return True
+    # return False
+
+    seen = {}
+    for i, val in enumerate(nums):
+        if val in seen and i - seen[val] <= k:
+            return True
+        else:
+            seen[val] = i
+
+    return False
+
+nums = [1,2,3,1]
+k=3
+print(containsNearbyDuplicate(nums,k)) #True
