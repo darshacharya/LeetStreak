6ee298fb07931da568a6132f9c7596b5a9561400
Sudarshan T S <chinnuacharya18@gmail.com>
2024-11-22 23:20:31 +0530

    """1636. Sort Array by Increasing Frequency"""

---
+++ Random/1636. Sort Array by Increasing Frequency.py
@@ -0,0 +1,11 @@
+"""1636. Sort Array by Increasing Frequency"""
+from itertools import count
+from collections import  Counter
+nums = [1,1,2,2,2,3]
+
+def frequencySort(nums):
+    count = Counter(nums)
+    ans = sorted(nums, key=lambda x: (count[x], -x))
+    return ans
+
+print(frequencySort(nums))
