ce3fd0214e38e7ada6127fed1ac6f37c737f6260
Sudarshan T S <chinnuacharya18@gmail.com>
2024-11-27 23:49:30 +0530

    344 reverse string in place

---
+++ 2 pointers/344. Reverse String.py
@@ -0,0 +1,12 @@
+"""344. Reverse String"""
+"""TC: O(N)"""
+"""SC: O(1)"""
+
+def reverseString(s):
+    # i,j=0, len(s)-1
+    # while i<=j:
+    #     s[i],s[j]=s[j],s[i]
+    #     i+=1
+    #     j-=1
+
+    s.reverse()
